f4bc2c9f32b64713fe9a2d4248b026ee04076abf
Deiu <andrei1.popescu1@gmail.com>
2024-03-13 14:55:08 +0200

    ui signup improvements

---
+++ src/components/home/DialogComp.tsx
@@ -0,0 +1,35 @@
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import TextField from "@mui/material/TextField";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import React from "react";
+import './DialogComp.css';
+
+interface DialogCompProps {
+    open: boolean,
+    onClick: () => void,
+
+    onSignup: () => void,
+}
+
+
+const DialogComp:React.FC<DialogCompProps> = (props) => {
+    return(
+        <Dialog open={props.open} onClose={props.onClick} className={"custom-dialog-paper"}>
+            <DialogTitle className={"custom-dialog-content"}>Sign Up</DialogTitle>
+            <DialogContent className={"custom-dialog-content"}>
+                <TextField autoFocus margin="dense" label="Name" type="text" fullWidth/>
+                <TextField margin="dense" label="Email Address" type="email" fullWidth />
+                <TextField margin="dense" label="Password" type="password" fullWidth/>
+            </DialogContent>
+            <DialogActions className={"custom-dialog-content"}>
+                <Button onClick={props.onClick} color="secondary">Cancel</Button>
+                <Button onClick={props.onSignup} color="primary">Sign Up</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default DialogComp;
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -7,6 +7,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import TextField from '@mui/material/TextField';
+import DialogComp from "./DialogComp";
 
 const Header = () => {
 
@@ -38,18 +39,7 @@ const Header = () => {
                 <a href="#">Login</a>
                 <Button variant="outlined" className="visualizer-button" onClick={handleOpen}>Try Visualizer for Free</Button>
             </div>
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Sign Up</DialogTitle>
-                <DialogContent>
-                    <TextField autoFocus margin="dense" label="Name" type="text" fullWidth />
-                    <TextField margin="dense" label="Email Address" type="email" fullWidth />
-                    <TextField margin="dense" label="Password" type="password" fullWidth />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="secondary">Cancel</Button>
-                    <Button onClick={handleSignUp} color="primary">Sign Up</Button>
-                </DialogActions>
-            </Dialog>
+            <DialogComp open={open} onClick={handleClose} onSignup={handleSignUp}/>
         </div>
     );
 };
